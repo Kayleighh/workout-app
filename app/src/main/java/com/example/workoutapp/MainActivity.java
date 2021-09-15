c29768f0c165462c75a24b569dda5440c53512cf
jurriaanVerhijde <jurriaan.verhijde@student.hu.nl>
2021-09-15 21:36:25 +0200

    Nieuwe versie dropdown bar toegevoegd

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.workoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Bedrijfs_Afdelingen, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+}
+class SpinnerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
