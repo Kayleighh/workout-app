892a4d477cdad943495a42f86ce0e144d4382b12
jurriaanVerhijde <jurriaan.verhijde@student.hu.nl>
2021-09-23 15:16:33 +0200

    Ready for merge 2.0

--- app/src/main/java/com/example/workoutapp/feature_Validatie_Scherm.java
+++ app/src/main/java/com/example/workoutapp/feature_Validatie_Scherm.java
@@ -6,16 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.sql.PreparedStatement;
 
 public class feature_Validatie_Scherm extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature_validatie_scherm);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        Spinner spinner =  findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.Bedrijfs_Afdelingen, android.R.layout.simple_spinner_item);
@@ -25,9 +32,16 @@ public class feature_Validatie_Scherm extends AppCompatActivity {
         spinner.setAdapter(adapter);
     }
     public void printData(View view) {
-        System.out.println(findViewById(R.id.editTextTextPersonName));
-        System.out.println(findViewById(R.id.editTextTextPersonName2));
-        System.out.println(findViewById(R.id.editTextNumber));
+        EditText edtName = findViewById(R.id.editTextTextPersonName);
+        EditText edtName2 = findViewById(R.id.editTextTextPersonName2);
+        EditText edtNumber = findViewById(R.id.editTextNumber);
+
+        String Name1 = edtName.getText().toString();
+        String Name2 = edtName2.getText().toString();
+        String Number = edtNumber.getText().toString();
+
+        System.out.println(Name1 + " " + Name2 + " " + Number);
+
 }
 class SpinnerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
