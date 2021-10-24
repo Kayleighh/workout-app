a3c4b418d5df140273e61c21ce1d30a581ab96ec
jurriaanVerhijde <jurriaan.verhijde@student.hu.nl>
2021-10-25 00:10:49 +0200

    Notificatie ready for merge

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.workoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +27,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 next();
             }
+
         });
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, 4);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+
+
+        startAlarm(c);
     }
 
     public void next(){
         Intent intent = new Intent(this, NotificationO2.class);
         startActivity(intent);
+    }
+
+
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
     }
 }
